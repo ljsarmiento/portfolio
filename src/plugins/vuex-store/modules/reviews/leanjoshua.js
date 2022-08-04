d613b4a1b81267feabd06209238a1cd102cef98f
Lean Joshua Sarmiento <sarmientoleanjoshua24@gmail.com>
2022-08-04 19:51:30 +0800

    Well done.

---
+++ src/plugins/vuex-store/modules/reviews/leanjoshua.js
@@ -0,0 +1,97 @@
+export default {
+    namespaced: true,
+
+    state: {
+
+        /**
+         * HEY! Thank you for exerting some efforts in reaching this JavaScript code.
+         * May the metaverse gods bless you now and forever!
+         *
+         * Let's get started, shall we?
+         *
+         * First, please fill in the 'personal' object below with your data
+         * Guide:
+         *        title      - your honorific or name title
+         *                     examples: 'Mr.', 'Ms.', 'Dr.', 'Engr.'
+         *                     this is OPTIONAL (leave empty if you want to stay stupid like me)
+         *
+         *        firstName  - your first name in proper case
+         *                     examples: 'John', 'Mary', 'Bill', 'Anne Billy'
+         *                     this is REQUIRED (or I will call you 'Poop', I'm not kidding)
+         *
+         *        middleName - your middle name in proper case
+         *                     examples: 'Delos', 'Reyes', 'Delos Reyes'
+         *                     this is OPTIONAL (no need to check your birth certificate)
+         *
+         *        lastName   - your last name or surname in proper case
+         *                     examples: 'Doe', 'Dela Cruz'
+         *                     this is REQUIRED (sorry, there's really a need to check your birth certificate)
+         *
+         *        nameSuffix - your name suffix
+         *                     examples: 'Jr.', 'III'
+         *                     this is OPTIONAL (leave empty if you don't have this extra baggage in your name)
+         *
+         */
+
+        // this is that 'personal' object I mentioned above:
+        personal: {
+            title     : 'Mr.',
+            firstName : 'Lean Joshua',
+            middleName: 'Tercenio',
+            lastName  : 'Sarmiento',
+            nameSuffix: '',
+        },
+        // ===================================================================================================
+
+
+        /**
+         * Next, please fill in the 'review' object below with your review on me.
+         * Guide:
+         *        as         - you're reviewing me as what?
+         *                     examples: 'friend', 'student', 'mentee', 'client', 'co-worker', 'boss'
+         *                     this is an ARRAY, so you can assign one or more values
+         *                     this is also REQUIRED (or I will tell Dr. Strange to delete you in my memory)
+         *
+         *        rating     - a number from 1 to 5
+         *                     examples: 4.5, 5
+         *                     this is REQUIRED (you know what to do)
+         *
+         *        content    - the actual review
+         *                     examples: (come on, examples? really?)
+         *                     this is REQUIRED (please be brutally honest)
+         *
+         */
+
+        // here you go:
+        review: {
+            as     : ['mentee', 'friend'],
+            rating : 5,
+            content: 'I can say that this mentor of us, is a person whose adept on his work and probably the best mentee I have met; He never fails to give or impart with us a new learnings each day. He definitely want all the best for us even in the downfall days of ourlives, He never fails to remind us that there is no shortcut to success, all the roads is surely long and arduous so he gives his very best to help us conquer every obstacles that might get through on our ways. So definitely, he is the great mentee and friend for me. '
+        },
+        // ===================================================================================================
+
+
+        /**
+         * Finally, please fill in the 'socials' object below with your social media links.
+         * Examples:
+         *           'https://github.com/kulotsystems',
+         *           'https://twitter.com/kulotsystems',
+         *           'https://web.facebook.com/kulotsystems'
+         */
+
+
+        // it's your time to shine!
+        socials: {
+            github  : 'https://github.com/ljsarmiento',
+            twitter : 'https://twitter.com/easymoneyleanj',
+            facebook: 'https://www.facebook.com/easymoneyleanj'
+        }
+        // ===================================================================================================
+
+
+        /**
+         * That's it!
+         * Thank you again!
+         */
+    }
+}
